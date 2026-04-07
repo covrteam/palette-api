69ab8e9571fb5f31a51ca4d61881c9c1141b4759
covrteam <bonjour@covrpack.com>
2026-04-07 16:20:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ def generate_palette():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.binary_location = "/usr/bin/chromium"
 
     driver = None
